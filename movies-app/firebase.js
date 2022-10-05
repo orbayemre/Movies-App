7d1fb94afa1f9699c7585328abaeaebf3de3c70d
orbayemre <emreorbay64@gmail.com>
2022-10-05 15:39:25 +0300

    update

--- movies-app/firebase.js
+++ movies-app/firebase.js
@@ -23,13 +23,13 @@ const errors ={
 
 
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain:  process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket:  process.env.storageBucket,
-    messagingSenderId:  process.env.messagingSenderId,
-    appId:  process.env.appId,
-    databaseURL: process.env.databaseURL,
+    apiKey: process.env["NEXT_PUBLIC_API_KEY "],
+    authDomain:  process.env["NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN "],
+    projectId: process.env["NEXT_PUBLIC_FIREBASE_PROJECT_ID "],
+    storageBucket:  process.env["NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET "],
+    messagingSenderId:  process.env["NEXT_PUBLIC_FIREBASE_MESSAGING_ID "],
+    appId:  process.env["NEXT_PUBLIC_FIREBASE_APP_ID "],
+    databaseURL: process.env["NEXT_PUBLIC_DATABASE_URL "],
 };
 
 const app = initializeApp(firebaseConfig);
